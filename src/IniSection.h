df63c7dbd7d12e7259c089fee7bc174faaea8d12
danila258 <sidorovdanila@gmail.com>
2023-03-31 18:32:58 +0300

    fix templates build

--- src/IniSection.h
+++ src/IniSection.h
@@ -23,33 +23,4 @@ private:
 };
 
 
-IniSection::IniSection(std::string  section, size_t index, size_t lineNum)
-        : _name(std::move(section)), _index(index), _lineNum(lineNum)
-{}
-
-IniSection::IniSection(const char* section, size_t index, size_t lineNum)
-    : _name(section), _index(index), _lineNum(lineNum)
-{}
-
-IniSection::operator std::string() const
-{
-    return _name;
-}
-
-std::string operator+(const std::string& str, const IniSection& section)
-{
-    return str + section._name;
-}
-
-size_t IniSection::getIndex() const
-{
-    return _index;
-}
-
-size_t IniSection::getLineNum() const
-{
-    return _lineNum;
-}
-
-
 #endif //INISECTION_H
